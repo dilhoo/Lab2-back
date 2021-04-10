4f5f1990ddce3535cb60e52f85dd3b2d05115dd5
dilhoo <dilholol2@gmail.com>
2021-04-10 17:02:02 +0300

    all back

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    java
+    war
+}
+
+group = "soa"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework:spring-webmvc:5.3.5")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.2")
+}
